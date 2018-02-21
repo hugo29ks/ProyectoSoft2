e260b38467069033ebf64fff88857d422bb9fa34
Ronny Cabrera <ronny-38@hotmail.com>
2018-02-21 10:37:43 -0500

    cambio de bsqueda de nombre por detalle

--- api/controllers/VistaController.js
+++ api/controllers/VistaController.js
@@ -36,7 +36,7 @@ module.exports = {
       .where({
         or: [
           {
-            nombreEvento: {
+            detalleEvento: {
               contains: parametros.busqueda
             }
           },
